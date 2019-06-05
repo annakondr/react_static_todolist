2cf64779b811122b8827346b1d8e6e47a6ace1cb
annakondr <tyukondra@gmail.com>
2019-06-05 16:24:34 +0300

    Updates

---
+++ precache-manifest.783e31b579832a427d81e73f77a0e3a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fdf2a70b04f221564a2dbc507f05fed",
+    "url": "/react_static_todolist/index.html"
+  },
+  {
+    "revision": "9d5826876913f4de572a",
+    "url": "/react_static_todolist/static/css/main.f473de23.chunk.css"
+  },
+  {
+    "revision": "94af7f8b07d7d4877212",
+    "url": "/react_static_todolist/static/js/2.15fbe00d.chunk.js"
+  },
+  {
+    "revision": "9d5826876913f4de572a",
+    "url": "/react_static_todolist/static/js/main.5f2f9c47.chunk.js"
+  },
+  {
+    "revision": "fd1786601d67f058b610",
+    "url": "/react_static_todolist/static/js/runtime~main.787e876d.js"
+  }
+]);
