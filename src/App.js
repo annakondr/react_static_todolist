132bdf81c364e58df8ae24f2b6c7ade6596fe091
annakondr <tyukondra@gmail.com>
2019-06-05 16:12:40 +0300

    solution

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import TodoList from './components/TodoList.js'
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+      <TodoList/>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {todos} from '../todos.js';
+import TodoItem from './TodoItem.js';
+import './TodoList.css';
+
+function TodoList() {
+const todoItemsList = [];
+  todos.forEach((item) => {
+    todoItemsList.push(<TodoItem title={item.title} completed={item.completed} userId={item.userId}/>)
+  });
+
+  return (
+    <table>
+      <thead>
+      <th>ToDoItem</th>
+      <th>User name</th>
+      <th>Completed</th>
+      </thead>
+      <tbody>
+      {todoItemsList}
+      </tbody>
+    </table>
+  )
+
+
+}
+
+export default TodoList;
